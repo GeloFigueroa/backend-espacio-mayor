a7c53a14a8d841bf19f0315fc34ab8dd96869d7b
GeloFigueroa <gfigueroa@conectamayor.cl>
2025-10-20 12:29:53 -0300

    correo más personalizado

--- resources/views/emails/tarjeta_por_vencer.blade.php
+++ resources/views/emails/tarjeta_por_vencer.blade.php
@@ -2,12 +2,13 @@
 # Aviso de vencimiento de tarjeta
 
 La tarjeta **{{ $tarjeta->titulo }}** vencerá el **{{ \Carbon\Carbon::parse($tarjeta->fecha_expiracion)->format('d/m/Y') }}**.  
-Por favor, tomen las medidas necesarias.
+
+Por favor, asegúrate de tomar las medidas necesarias antes de su fecha de vencimiento para mantener todo actualizado.
 
 <x-mail::button :url="url('/tarjetas')">
 Ver Tarjetas
 </x-mail::button>
 
-Gracias,<br>
-{{ config('app.name') }}
+Gracias por confiar en **Espacio Mayor**.<br>
+**El equipo de Espacio Mayor**
 </x-mail::message>
